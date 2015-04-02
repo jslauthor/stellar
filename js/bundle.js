7a1b6c847f352e8cf44fc1561453e2088d9a9ba8
Leonard Souza <leonard.souza@spatialkey.com>
2015-04-02 08:40:55 -0700

    fixed svg masking issue

--- js/bundle.js
+++ js/bundle.js
@@ -55,7 +55,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "4add47215c52ffc96403";
+/******/ 	var hotCurrentHash = "6fe059ab5ea4efea18d1";
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = [];
 /******/ 	
@@ -18737,7 +18737,7 @@
 /* 89 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = "\"use strict\";\n\nvar React = require('react')\nvar ConfigStore = require('../../stores/ConfigStore')\n\nvar Stars = React.createClass({displayName: \"Stars\",\n    getDefaultProps: function() {\n        return {\n            stars: 0\n        }\n    },\n    render: function() {\n\n        var percent = ((this.props.stars > 0 ? Math.min(this.props.stars, 5) / 5 : 0) * 100) + \"%\";\n\n        return (\n            React.createElement(\"svg\", {viewBox: \"0 0 73 12\", width: \"73\", height: \"12\"}, \n                React.createElement(\"defs\", null, \n                    React.createElement(\"clipPath\", {id: \"starMask\"}, \n                        React.createElement(\"path\", {x: \"0\", y: \"0\", fill: \"#FFFFFF\", d: \"M8.6,3.8L6.5,0L4.4,3.8L0,4.6l3,3.1L2.5,12l4-1.8l4,1.8L10,7.7\" + ' ' +\n\t\t\t\"l3-3.1L8.6,3.8z M23.6,3.8L21.5,0l-2.1,3.8L15,4.6l3,3.1L17.5,12l4-1.8l4,1.8L25,7.7l3-3.1L23.6,3.8z M38.6,3.8L36.5,0l-2.1,3.8\" + ' ' +\n\t\t\t\"L30,4.6l3,3.1L32.5,12l4-1.8l4,1.8L40,7.7l3-3.1L38.6,3.8z M53.6,3.8L51.5,0l-2.1,3.8L45,4.6l3,3.1L47.5,12l4-1.8l4,1.8L55,7.7\" + ' ' +\n\t\t\t\"l3-3.1L53.6,3.8z M73,4.6l-4.4-0.8L66.5,0l-2.1,3.8L60,4.6l3,3.1L62.5,12l4-1.8l4,1.8L70,7.7L73,4.6z\"})\n                    )\n                ), \n\n                React.createElement(\"g\", {style: { clipPath: \"url(#starMask)\"}, width: \"100%\", height: \"100%\"}, \n                    React.createElement(\"rect\", {x: \"0\", y: \"0\", width: \"100%\", height: \"100%\", fill: \"#\"+ConfigStore.getDarkGreen()}), \n                    React.createElement(\"rect\", {x: \"0\", y: \"0\", width: percent, height: \"100%\", fill: \"#\"+ConfigStore.getGreen()})\n                )\n\n            )\n        )\n    }\n})\n\nmodule.exports = Stars;\n"
+	module.exports = "\"use strict\";\n\nvar React = require('react')\nvar ConfigStore = require('../../stores/ConfigStore')\n\nvar Stars = React.createClass({displayName: \"Stars\",\n    getDefaultProps: function() {\n        return {\n            stars: 0\n        }\n    },\n    render: function() {\n\n        var percent = ((this.props.stars > 0 ? Math.min(this.props.stars, 5) / 5 : 0) * 100) + \"%\";\n\n        return (\n            React.createElement(\"svg\", {viewBox: \"0 0 73 12\", width: \"73\", height: \"12\"}, \n\n                React.createElement(\"path\", {x: \"0\", y: \"0\", fill: \"#\"+ConfigStore.getDarkGreen(), d: \"M8.6,3.8L6.5,0L4.4,3.8L0,4.6l3,3.1L2.5,12l4-1.8l4,1.8L10,7.7\" + ' ' +\n\t\t\t\"l3-3.1L8.6,3.8z M23.6,3.8L21.5,0l-2.1,3.8L15,4.6l3,3.1L17.5,12l4-1.8l4,1.8L25,7.7l3-3.1L23.6,3.8z M38.6,3.8L36.5,0l-2.1,3.8\" + ' ' +\n\t\t\t\"L30,4.6l3,3.1L32.5,12l4-1.8l4,1.8L40,7.7l3-3.1L38.6,3.8z M53.6,3.8L51.5,0l-2.1,3.8L45,4.6l3,3.1L47.5,12l4-1.8l4,1.8L55,7.7\" + ' ' +\n\t\t\t\"l3-3.1L53.6,3.8z M73,4.6l-4.4-0.8L66.5,0l-2.1,3.8L60,4.6l3,3.1L62.5,12l4-1.8l4,1.8L70,7.7L73,4.6z\"}), \n\n                React.createElement(\"svg\", {width: percent, height: \"100%\", style: {overflow:\"hidden\"}}, \n                    React.createElement(\"path\", {x: \"0\", y: \"0\", fill: \"#\"+ConfigStore.getGreen(), d: \"M8.6,3.8L6.5,0L4.4,3.8L0,4.6l3,3.1L2.5,12l4-1.8l4,1.8L10,7.7\" + ' ' +\n\t\t\t\"l3-3.1L8.6,3.8z M23.6,3.8L21.5,0l-2.1,3.8L15,4.6l3,3.1L17.5,12l4-1.8l4,1.8L25,7.7l3-3.1L23.6,3.8z M38.6,3.8L36.5,0l-2.1,3.8\" + ' ' +\n\t\t\t\"L30,4.6l3,3.1L32.5,12l4-1.8l4,1.8L40,7.7l3-3.1L38.6,3.8z M53.6,3.8L51.5,0l-2.1,3.8L45,4.6l3,3.1L47.5,12l4-1.8l4,1.8L55,7.7\" + ' ' +\n\t\t\t\"l3-3.1L53.6,3.8z M73,4.6l-4.4-0.8L66.5,0l-2.1,3.8L60,4.6l3,3.1L62.5,12l4-1.8l4,1.8L70,7.7L73,4.6z\"})\n                )\n\n            )\n        )\n    }\n})\n\nmodule.exports = Stars;\n"
 
 /***/ },
 /* 90 */
@@ -18870,18 +18870,17 @@
 	
 	        return (
 	            React.createElement("svg", {viewBox: "0 0 73 12", width: "73", height: "12"}, 
-	                React.createElement("defs", null, 
-	                    React.createElement("clipPath", {id: "starMask"}, 
-	                        React.createElement("path", {x: "0", y: "0", fill: "#FFFFFF", d: "M8.6,3.8L6.5,0L4.4,3.8L0,4.6l3,3.1L2.5,12l4-1.8l4,1.8L10,7.7" + ' ' +
+	
+	                React.createElement("path", {x: "0", y: "0", fill: "#"+ConfigStore.getDarkGreen(), d: "M8.6,3.8L6.5,0L4.4,3.8L0,4.6l3,3.1L2.5,12l4-1.8l4,1.8L10,7.7" + ' ' +
+				"l3-3.1L8.6,3.8z M23.6,3.8L21.5,0l-2.1,3.8L15,4.6l3,3.1L17.5,12l4-1.8l4,1.8L25,7.7l3-3.1L23.6,3.8z M38.6,3.8L36.5,0l-2.1,3.8" + ' ' +
+				"L30,4.6l3,3.1L32.5,12l4-1.8l4,1.8L40,7.7l3-3.1L38.6,3.8z M53.6,3.8L51.5,0l-2.1,3.8L45,4.6l3,3.1L47.5,12l4-1.8l4,1.8L55,7.7" + ' ' +
+				"l3-3.1L53.6,3.8z M73,4.6l-4.4-0.8L66.5,0l-2.1,3.8L60,4.6l3,3.1L62.5,12l4-1.8l4,1.8L70,7.7L73,4.6z"}), 
+	
+	                React.createElement("svg", {width: percent, height: "100%", style: {overflow:"hidden"}}, 
+	                    React.createElement("path", {x: "0", y: "0", fill: "#"+ConfigStore.getGreen(), d: "M8.6,3.8L6.5,0L4.4,3.8L0,4.6l3,3.1L2.5,12l4-1.8l4,1.8L10,7.7" + ' ' +
 				"l3-3.1L8.6,3.8z M23.6,3.8L21.5,0l-2.1,3.8L15,4.6l3,3.1L17.5,12l4-1.8l4,1.8L25,7.7l3-3.1L23.6,3.8z M38.6,3.8L36.5,0l-2.1,3.8" + ' ' +
 				"L30,4.6l3,3.1L32.5,12l4-1.8l4,1.8L40,7.7l3-3.1L38.6,3.8z M53.6,3.8L51.5,0l-2.1,3.8L45,4.6l3,3.1L47.5,12l4-1.8l4,1.8L55,7.7" + ' ' +
 				"l3-3.1L53.6,3.8z M73,4.6l-4.4-0.8L66.5,0l-2.1,3.8L60,4.6l3,3.1L62.5,12l4-1.8l4,1.8L70,7.7L73,4.6z"})
-	                    )
-	                ), 
-	
-	                React.createElement("g", {style: { clipPath: "url(#starMask)"}, width: "100%", height: "100%"}, 
-	                    React.createElement("rect", {x: "0", y: "0", width: "100%", height: "100%", fill: "#"+ConfigStore.getDarkGreen()}), 
-	                    React.createElement("rect", {x: "0", y: "0", width: percent, height: "100%", fill: "#"+ConfigStore.getGreen()})
 	                )
 	
 	            )
